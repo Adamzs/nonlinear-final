0d84ca2919b5e6f2eab6e6c5dfe3454b6ea1c4d9
William <morelwmilliam@gmail.com>
2017-05-31 22:51:30 -0500

    Commenting

--- kchoice.R
+++ kchoice.R
@@ -109,25 +109,25 @@ mlogitPredicted <- as.integer(apply(mlogitPredicted,1,function(x) names(which(x=
 
 mlogitmisclassification <- mean(as.character(mlogitPredicted) != as.character(finaldata$choiceLabel))
 
-gamweekloyaltypriceImodel <- gam(list(choice~Combined.Pre.Tax.Income.of.HH + 
-                                       MALE_SMOKE + FEM_SMOKE + s(WEEK) + 
-                                       s(LOYALTY.10158) + s(AVGRATE.10158) + te(AVGRATE.10158, WEEK, k=8) + te(LOYALTY.10158, AVGRATE.10158, k=8) + 
-                                       DISPLAY.10158 + s(AVGRATE.10158, by=FEATURE.10158), 
-                                     ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
-                                       FEM_SMOKE + s(WEEK) +  s(LOYALTY.33200) + s(AVGRATE.33200) + 
-                                       te(AVGRATE.33200, WEEK, k=8) +  te(AVGRATE.33200, LOYALTY.33200, k=8) +
-                                       DISPLAY.33200 + s(AVGRATE.33200, by=FEATURE.33200),
-                                     ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
-                                       FEM_SMOKE + s(WEEK) + s(LOYALTY.37000) + 
-                                       s(AVGRATE.37000) + DISPLAY.37000 + te(AVGRATE.37000, WEEK, k=8) +
-                                       te(AVGRATE.37000, LOYALTY.37000, k=8) + s(AVGRATE.37000, by=FEATURE.37000), 
-                                     ~Combined.Pre.Tax.Income.of.HH + 
-                                       MALE_SMOKE + FEM_SMOKE + s(WEEK) +
-                                       s(LOYALTY.53100) + s(AVGRATE.53100) + te(AVGRATE.53100, WEEK, k=8) +
-                                       DISPLAY.53100 + te(AVGRATE.53100, LOYALTY.53100, k=8) + s(AVGRATE.53100, by=FEATURE.53100), 
-                                     ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
-                                       FEM_SMOKE +  s(WEEK) + s(LOYALTY.77326) + 
-                                       s(AVGRATE.77326) + DISPLAY.77326 + te(LOYALTY.77326, AVGRATE.77326, k=8) +
-                                       te(WEEK, AVGRATE.77326, k=8) + s(AVGRATE.77326, by=FEATURE.77326)), data=finaldata, family=multinom(K=5))
-summary(mnlmodel)
-plot(gamweekloyaltypricemodel)
+# gamweekloyaltypriceImodel <- gam(list(choice~Combined.Pre.Tax.Income.of.HH + 
+#                                        MALE_SMOKE + FEM_SMOKE + s(WEEK) + 
+#                                        s(LOYALTY.10158) + s(AVGRATE.10158) + te(AVGRATE.10158, WEEK, k=8) + te(LOYALTY.10158, AVGRATE.10158, k=8) + 
+#                                        DISPLAY.10158 + s(AVGRATE.10158, by=FEATURE.10158), 
+#                                      ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
+#                                        FEM_SMOKE + s(WEEK) +  s(LOYALTY.33200) + s(AVGRATE.33200) + 
+#                                        te(AVGRATE.33200, WEEK, k=8) +  te(AVGRATE.33200, LOYALTY.33200, k=8) +
+#                                        DISPLAY.33200 + s(AVGRATE.33200, by=FEATURE.33200),
+#                                      ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
+#                                        FEM_SMOKE + s(WEEK) + s(LOYALTY.37000) + 
+#                                        s(AVGRATE.37000) + DISPLAY.37000 + te(AVGRATE.37000, WEEK, k=8) +
+#                                        te(AVGRATE.37000, LOYALTY.37000, k=8) + s(AVGRATE.37000, by=FEATURE.37000), 
+#                                      ~Combined.Pre.Tax.Income.of.HH + 
+#                                        MALE_SMOKE + FEM_SMOKE + s(WEEK) +
+#                                        s(LOYALTY.53100) + s(AVGRATE.53100) + te(AVGRATE.53100, WEEK, k=8) +
+#                                        DISPLAY.53100 + te(AVGRATE.53100, LOYALTY.53100, k=8) + s(AVGRATE.53100, by=FEATURE.53100), 
+#                                      ~Combined.Pre.Tax.Income.of.HH + MALE_SMOKE + 
+#                                        FEM_SMOKE +  s(WEEK) + s(LOYALTY.77326) + 
+#                                        s(AVGRATE.77326) + DISPLAY.77326 + te(LOYALTY.77326, AVGRATE.77326, k=8) +
+#                                        te(WEEK, AVGRATE.77326, k=8) + s(AVGRATE.77326, by=FEATURE.77326)), data=finaldata, family=multinom(K=5))
+# summary(mnlmodel)
+# plot(gamweekloyaltypricemodel)
